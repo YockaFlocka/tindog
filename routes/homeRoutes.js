570a15b272eac8920ff8e0d2f0035cf7e8914172
Jhelt <jonhelton405@gmail.com>
2023-05-13 12:51:50 -0400

    added home routes and updated matches and users handlebars pages

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
 
-
 router.get('/', (req, res) => {
   res.render('all')
+})
+
+router.get('/matches', (req, res) => {
+  res.render('matches')
+})
+
+router.get('/users', (req, res) => {
+  res.render('users')
 })
 
 module.exports = router;
