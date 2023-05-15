165f7421461e74286a7ab83acc7cda3356bc4f1a
Peter Yockey <peterjyockey@gmail.com>
2023-05-15 11:05:57 -0500

    added login/signup page

--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -12,4 +12,14 @@ router.get('/users', (req, res) => {
   res.render('users')
 })
 
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  // if (req.session.logged_in) {
+  //   res.redirect('/profile');
+  //   return;
+  // }
+
+  res.render('login');
+});
+
 module.exports = router;
