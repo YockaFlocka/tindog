e7557887326766e4469b78800025c47dc44a8dfc
Ian Bui <thinkbui@gmail.com>
2023-05-14 21:40:55 -0700

    Added first favorite seeds

--- seeds/index.js
+++ seeds/index.js
@@ -3,6 +3,7 @@ const {Favorite, Reference, User} = require('../models');
 
 const userData = require('./userData.json');
 const breedData = require('./breedData.json');
+const favoriteData = require('./favoriteData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -13,6 +14,19 @@ const seedDatabase = async () => {
   });
 
   await Reference.bulkCreate(breedData);
+
+  for (const favorite of favoriteData) {
+    const ref = await Reference.findOne({ where: { name: favorite.reference_name } });
+    if (ref) {
+      const {reference_name, ...favorite_sub} = favorite
+      await Favorite.create({
+        ...favorite_sub,
+        reference_id: ref.id
+      })
+    } else {
+      console.log(`WARNING: '${favorite.reference_name}' not found.  Skipping...`)
+    }
+  }
 
   process.exit(0);
 };
