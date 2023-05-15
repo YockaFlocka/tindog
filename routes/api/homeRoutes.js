408cfdb00c4348068471e736e67626048e030d11
Ian Bui <thinkbui@gmail.com>
2023-05-15 16:22:02 -0700

    Moved url decode to helper file

--- routes/api/homeRoutes.js
+++ routes/api/homeRoutes.js
@@ -1,23 +1,19 @@
 const router = require("express").Router();
 // const https = require("https");
+const get_breed_from_url = require("../../utils/helpers")
 
 const url = 'https://dog.ceo/api/breeds/image/random/10';
 
 router.get("/", (req, res) => {
-  const getBreed = dog =>
-    dog.message.split("breeds/")[1].split("/")[0]
-
-  // let sent = false
-  // while (!sent) {
-    fetch(url)
-      .then(res => res.json())
-      .then(data => {
-        // if (getBreed(data) === "kuvasz") {
-          res.send(data)
-          // sent = true
-        // }
-      })
-  // }
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      for(i=0;i<data.message.length;i++){
+        console.log(get_breed_from_url(data.message[i]))
+      }
+      console.log(data.message.map((elem) => get_breed_from_url(elem)))
+      res.send(data)
+    })
 })
 
 module.exports = router;
