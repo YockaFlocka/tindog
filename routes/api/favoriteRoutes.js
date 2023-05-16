4ae789075ad570563600bd6ea0039a5eaf0a4e3f
Ian Bui <thinkbui@gmail.com>
2023-05-16 12:14:44 -0700

    Added version of post route that uses dog_ceo_code instead of reference_id

--- routes/api/favoriteRoutes.js
+++ routes/api/favoriteRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Favorite = require("../../models/Favorite");
+const { Favorite, Reference } = require("../../models");
 
 
 router.get("/", (req, res) => {
@@ -8,8 +8,19 @@ router.get("/", (req, res) => {
     .catch( err => res.json({ msg: err.message }))
 })
 
-router.post("/", (req, res) => {
-  Favorite.create(req.body)
+// Version with reference_id
+// router.post("/", (req, res) => {
+//   Favorite.create(req.body)
+//     .then( resp => res.json({ status: "success", payload: resp }))
+//     .catch( err => res.json({ msg: err.message }))
+// })
+
+// Version with dog_ceo_code
+router.post("/", async (req, res) => {
+  const ref = await Reference.findOne({ where: { dog_ceo_code: req.body.dog_ceo_code } })
+  let {dog_ceo_code: _, ...data} = req.body
+  data.reference_id = ref.id
+  Favorite.create(data)
     .then( resp => res.json({ status: "success", payload: resp }))
     .catch( err => res.json({ msg: err.message }))
 })
