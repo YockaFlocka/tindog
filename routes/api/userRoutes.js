8618261a9ba8ba95f296ae93fae2b883cdf8c845
Peter Yockey <peterjyockey@gmail.com>
2023-05-16 12:21:09 -0500

    fixed logout pathing problems

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -78,6 +78,7 @@ router.post('/login', async (req, res) => {
 });
 
 router.post('/logout', (req, res) => {
+  console.log(req.session)
   if (req.session.logged_in) {
     req.session.destroy(() => {
       res.status(204).end();
