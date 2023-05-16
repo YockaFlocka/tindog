78b2314da999f29b6ad9847fc4212346641629d6
Ian Bui <thinkbui@gmail.com>
2023-05-15 20:25:40 -0700

    Completed random route for one random image

--- routes/api/homeRoutes.js
+++ routes/api/homeRoutes.js
@@ -1,18 +1,27 @@
 const router = require("express").Router();
-// const https = require("https");
+const { Reference } = require("../../models");
 const get_breed_from_url = require("../../utils/helpers")
 
-const url = 'https://dog.ceo/api/breeds/image/random/10';
+const url = 'https://dog.ceo/api/breeds/image/random';
 
 router.get("/", (req, res) => {
   fetch(url)
     .then(res => res.json())
     .then(data => {
-      for(i=0;i<data.message.length;i++){
-        console.log(get_breed_from_url(data.message[i]))
-      }
-      console.log(data.message.map((elem) => get_breed_from_url(elem)))
-      res.send(data)
+      const breed = get_breed_from_url(data.message)
+      Reference.findOne({ where: { dog_ceo_code: breed } })
+        .then((result) => {
+          console.log(result)
+          const exp_data = {
+            message: {
+              photo_url: data.message,
+              ref_code: breed,
+              ref_id: result ? result.id : -1,
+              adoptable: result ? result.whitelist : false
+            }
+          }
+          res.send(exp_data)
+        })
     })
 })
 
